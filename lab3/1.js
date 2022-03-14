d7a5b0777433e1c37f84cf567b92288891c109c9
en1ba <eliseevis0206.gmail.com>
2022-03-14 21:55:05 +0300

    fix

--- lab3/1.js
+++ lab3/1.js
@@ -8,7 +8,7 @@
 function capitalize(str) {
     let str2 = '';
     for (let v of str.split(' ')) {
-        str2 = str2 + String(v.replace(v[0], v[0].toUpperCase()))+' ';
+        str2 =  str2 + String(v.replace(v[0], v[0].toUpperCase()))+' ';
     }
     str2 = str2.substr(0, str2.length - 1);
     return str2;
